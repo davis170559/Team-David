6916a6a41fe7da7fbf3d68ca84bc59efc852a70e
David Espinoza <170559@upslp.edu.mx>
2021-10-10 22:23:04 -0500

    conexión
    se realizo la conexión con una base de datos local y se autentica el usuario que se ingresa

--- TelegramBot/main.py
+++ TelegramBot/main.py
@@ -1,11 +1,28 @@
 import os
 import telebot
-import pymongo
 import time
+import pymysql
 import urllib
 
+commands = {  # command description used in the "help" command
+    'start': 'Empieza a usar el bot',
+    'help': 'Información de inicio',
+    'auth':'iniciar con la autentificación'
+    
+}
+
+comm_log = {
+    'ayuda':'Información acerca del bot',
+    'placas' : 'Buscar por placas',
+    'nombre': 'Buscar por nombre'
+
+}
+
+
+"""
 knownUsers = []  # todo: save these in a file,
 userStep = {}  # so they won't reset every time the bot restarts
+
 User = os.environ['USER']
 Pass = os.environ['pass']
 
@@ -14,26 +31,40 @@ client = pymongo.MongoClient(
     "@clusterpepo.saoby.mongodb.net/ClusterPepo?retryWrites=true&w=majority")
 db = client.ClusterPepo
 print(db.list_collection_names())
-
-API_KEY = os.environ['API_KEY']
+"""
+dicc={}  
+API_KEY = "2034503541:AAGIeh9RmLHhJuVzwMSMk5pQ0PZMYs8OrsQ"
 bot = telebot.TeleBot(API_KEY)
+#mysql databse
+class Database:
+    def __init__(self):
+        self.connection=pymysql.connect( 
+            
+            host='localhost', #or add ip
+            user='root',
+            password='',
+            db='bd_placas'
+        )
 
-commands = {  # command description used in the "help" command
-    'start': 'Empieza a usar el bot',
-    'help': 'Informacion acerca de los comandos del bot',
-    'placas': 'Buscar por placas',
-    'nombre': 'Buscar por nombre'
-}
+        self.cursor =self.connection.cursor()
+
+        print("conexion estavblecida")
+
+    def select_user(self,id,m) -> bool:
+        sql='SELECT id_usr FROM usuarios WHERE id_usr = \''+ str(id)+'\'' 
+
+        try:
+            self.cursor.execute(sql)
+            user = self.cursor.fetchone()
+            if(user[0]==id):
+                return True
+        except Exception as e:
+            bot.send_message(m.chat.id, "No se puede autenticar en este momento, por favor contacte con el"+ 
+            " administrador de este bot")
 
 
-def get_user_step(uid):
-    if uid in userStep:
-        return userStep[uid]
-    else:
-        knownUsers.append(uid)
-        userStep[uid] = 0
-        print("New user detected, who hasn't used \"/start\" yet")
-        return 0
+database= Database()
+
 
 
 @bot.message_handler(commands=['start', 'hola', 'Hola'])
@@ -42,12 +73,49 @@ def greet(message):
     #bot.reply_to(message,"Hola, bienvenido")
     #user = update.message.from_user
     bot.send_message(message.chat.id, "Hola " + name + " bienvenido!!")
-    command_help(message)
+    #command_help(message)
 
+
+#ayuda de inicio, before login
+
+@bot.message_handler(commands=['help'])
+def command_hel(m):
+    cid = m.chat.id
+    help_text = "Para iniciar con este bot tienes que autenticarte con el comando /auth"
+    bot.send_message(cid, help_text)  # send the generated help page
+
+#auth
+@bot.message_handler(commands=['auth'])
+def command_help(m):
+    cid = m.chat.id
+    mes = "Ingresa tu usuario"
+    bot.send_message(cid, mes)  # send the generated help page
+
+    dicc[cid]=1
+    print(dicc)
+
+    
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    cid = message.chat.id
+    mid = message.message_id 
+    message_text = message.text 
+    user_id = message.from_user.id 
+    user_name = message.from_user.first_name 
+
+    print(dicc)
+    print(message_text)
+
+    if dicc.get(cid) == 1:
+        print("autenticando")
+        bot.send_message(message.chat.id, "autenticando...")
+        if (database.select_user(message_text,message)):
+            bot.send_message(message.chat.id, "usuario encontrado!")
+        
 
 # help page
-@bot.message_handler(commands=['help'])
-def command_help(m):
+@bot.message_handler(commands=['ayuda'])
+def command_a(m):
     cid = m.chat.id
     help_text = "Puedes usar los siguiente comandos: \n"
     for key in commands:  # generate help text out of the commands dictionary defined at the top
@@ -80,11 +148,24 @@ def nom(message):
     send_photo(message)
     
 
-
+'''
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def command_default(m):
     # this is the standard reply to a normal message
     bot.send_message(
         m.chat.id, "No entiendo \"" + m.text +
         "\"\nUsa el comando /help para conocer como utilizar el bot")
+
+
+
+def get_user_step(uid):
+    if uid in userStep:
+        return userStep[uid]
+    else:
+        knownUsers.append(uid)
+        userStep[uid] = 0
+        print("New user detected, who hasn't used \"/start\" yet")
+        return 0
+'''
+
 bot.polling()
